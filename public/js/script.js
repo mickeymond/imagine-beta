68af1dedda0ffe5a7f0a3391549c61f4b727330e
Michael Hammond <mickeymond@gmail.com>
2024-06-06 11:44:57 +0000

    Math in JavaScript

--- public/js/script.js
+++ public/js/script.js
@@ -59,3 +59,36 @@ for (let i = 0; i < bottles.length; i++) {
     // console.log('We did it!', i);
     console.log(bottles[i]);
 }
+
+// Functions
+// Defining a function
+function login(username, password) {
+    // Validate username and password
+    if (!username || !password) {
+      return 'Username or password not provided';
+    }
+    // Verify username and password
+    if (username == 'mickeymond' && password == '1234') {
+      return 'User is logged in';
+    } else {
+      return 'Invalid username or password';
+    }
+  }
+  // Invoking a function
+  login('mickeymond');
+  
+  
+  // Basic Arithmetic Operations
+  11 + 12;
+  1 + 0.5;
+  43 - 12;
+  45 / 7;
+  5 * 3;
+  14 % 3;
+  
+  Math.floor(45 / 24);
+  45 % 24
+  Math.random() * 1000;
+  Math.max(34,32,37);
+  2 * (3 + 4) - 5 / 2;
+  Number('ty') + 2
